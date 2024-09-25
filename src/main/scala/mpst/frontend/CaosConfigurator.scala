86222c7b908ce82608b5ea250fbba962c04eec0c
Telmo André Azevedo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2024-09-25 20:49:37 +0100

    setting (seting up)

--- src/main/scala/mpst/frontend/CaosConfigurator.scala
+++ src/main/scala/mpst/frontend/CaosConfigurator.scala
@@ -18,25 +18,12 @@ import mpst.operational_semantic.MPSTSemanticWrapper.*
 import mpst.syntax.Keyword.{ComAsyncMS, ComSync, InterleaveOff, RecKleene}
 
 object CaosConfigurator extends Configurator[Configuration]:
-  // var usedProjection = Void
-  // var usedSos        = Void
-  // var usedRec        = Void
-  // var usedInter      = Void
-
   override val name:String = "CoMPSeT - Comparison of Multiparty Session Types"
 
   override val languageName:String = "session"
 
   override val parser:String=>Configuration = parseAllWrapper
 
-  override val examples:Seq[Example] = List(
-    "MasterWorkers"
-      -> "m>wA:Work ; m>wB:Work ; (wA>m:Done || wB>m:Done)",
-
-    "SimpleRecursion"
-      -> "def X in (m>w:Task ; X)"
-  )
-  
   /*
   override val options:Seq[(String,String)] = List(
     "Communication Type" -> (AsyncMS,AsyncCS,Sync,Multicast),
@@ -45,17 +32,17 @@ object CaosConfigurator extends Configurator[Configuration]:
   )
   */
 
-  override val widgets:Seq[(String,WidgetInfo[Configuration])] = List(
-    "testing multi steps"
-      -> stepsWithConfig(
-      initialSt = (config:Configuration) =>
-        val global -> _ = config
-        global -> Environment.globalEnv(global),
-      SOSs = List(MPSTSemanticWrapper),
-      viewSt = (pro:Protocol,env:Map[Variable,Protocol]) => pro.toString,
-      typ = Text,
-    ),
+  override val setting: Setting[Configuration] = ???
+  
+  override val examples:Seq[Example] = List(
+    "MasterWorkers"
+      -> "m>wA:Work ; m>wB:Work ; (wA>m:Done || wB>m:Done)",
 
+    "SimpleRecursion"
+      -> "def X in (m>w:Task ; X)"
+  )
+  
+  override val widgets:Seq[(String,WidgetInfo[Configuration])] = List(
     "parsed configuration"
       -> view(
       viewProg = (config:Configuration) =>
@@ -141,4 +128,39 @@ object CaosConfigurator extends Configurator[Configuration]:
     then "WELL FORMED!"
     else "NOT WELL FORMED!"
   end wellFormedness
+
+  /* BRAINSTORM:
+    -- BOTTOM UP DESIGN --
+      choice A = (choiceName, widgetsList, render) =>
+        a tuple associating a name, the widgets necessary to implement said choice, and a boolean stating if it should be rendered
+        ex: asyncMSChoice = ("Async - MultiSet", List(steps(...)), true)
+
+      option A = (optionName, choiceList, render) =>
+        a tuple associating a name, the choices available for said option, and a boolean stating if it should be rendered
+        ex: communicationOption = ("Communication Model", List(asyncMSChoice, asyncCSChoice, syncChoice), true)
+
+      config A = (configName, optionList, render) =>
+        a tuple associating a name, the options available for said config, and a boolean stating if it should be rendered
+        ex: config1 = ("Config A", List(communicationOption, InterleavingOption, ...), true)
+
+      setting A = List(config) =>
+        a list (tuple if binary) of configurations
+        we can override it within the CaosConfigurator
+        caos builds the UI knowing this structure
+        the "render" arguments reflects on checkboxes the final user can enable or not (similar to the already present collapse system)
+          essentially collapsing large sections to eliminate visual pollution
+        there would be a "reload" option the final user could press after checking/unchecking boxes
+
+        PROBLEMS:
+          - is this to much?
+            although it reflects almost 1:1 the visual structure as seen by the final user
+              there are many categories: setting, config, options, choices, widgets
+            the render choice will allow to prune entire sections when parsing the structure
+            but the person implementing this structure in his caos configurator will need to write a lot...
+              defining widgets, to define choices, to define options, to define configs, to alas define a setting that the caos will deal with
+          - are there any problems that I am not foreseeing with implementing this on caos?
+          - I am using lists of stuff for the "sake" of simplicity, the possibility of this being implemented in something
+            like a tree is not lost on me, but then what about the different "tiers"
+  */
 end CaosConfigurator
+
