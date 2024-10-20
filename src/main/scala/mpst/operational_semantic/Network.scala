775cee7aba1abfcf5e35367b23a075dfc473beba
Telmo André Azevedo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2024-10-20 23:49:46 +0100

    work to do

--- src/main/scala/mpst/operational_semantic/Network.scala
+++ src/main/scala/mpst/operational_semantic/Network.scala
@@ -56,39 +56,6 @@ object Network:
     end notBlocked
   end NetworkMultiset
   /*
-  object NetworkCausal:
-    def nextNetwork(locals:Set[(Agent,Local)],pending:Queue)(using environment:Map[Agent,Map[Variable,Local]]):Set[(Action,Set[(Agent,Local)],Queue)] =
-      val nextNetwork = for local <- locals yield
-        val nextEntry = getNextEntry(local,pending)
-        for (nextAction,nextLocal,nextPending) <- nextEntry yield
-          val nextLocals = locals-local+nextLocal
-          (nextAction,nextLocals,nextPending)
-      nextNetwork.flatten
-    end nextNetwork
-
-    private def getNextEntry(local:(Agent,Local),pending:Queue)(using environment:Map[Agent,Map[Variable,Protocol]]):Set[(Action,(Agent,Local),Queue)] =
-      for nextAction -> nextLocal <- MPSTSemantic.next(local._2)(using environment(local._1)) if notBlocked(nextAction,pending) yield
-        val nextPending = getNextPending(nextAction,pending)
-        (nextAction,local._1 -> nextLocal,nextPending)
-    end getNextEntry
-
-    private def getNextPending(action:Action,pending:Queue):Queue =
-      action match
-        case Send(agentA,agentB,message,_) =>
-          val entry = (agentA -> agentB) -> (pending.getOrElse(agentA -> agentB,Nil):::List(message))
-          pending + entry
-        case Receive(agentA,agentB,_,_) =>
-          val entry = (agentB -> agentA) -> pending(agentB -> agentA).tail
-          pending + entry
-        case protocol => throw new RuntimeException(s"unexpected protocol found in [$protocol]\n")
-    end getNextPending
-
-    private def notBlocked(action:Action,network:Queue):Boolean =
-      action match
-        case Send(_,_,_,_) => true
-        case Receive(agentA,agentB,message,sort) => (network contains agentB -> agentA) && network(agentB -> agentA).nonEmpty && network(agentB -> agentA).head == message
-        case protocol => throw new RuntimeException(s"unexpected protocol found in [$protocol]\n")
-    end notBlocked
-  end NetworkCausal
+    TACKLE CAUSAL
   */
 end Network
