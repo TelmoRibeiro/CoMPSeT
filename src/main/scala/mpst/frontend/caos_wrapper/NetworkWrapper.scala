f3046ca2efc49f58d75ece3492a4efd5ad1fb093
Telmo André Azevedo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2025-02-04 13:01:11 +0000

    Network Causal with Session Queue

--- src/main/scala/mpst/frontend/caos_wrapper/NetworkWrapper.scala
+++ src/main/scala/mpst/frontend/caos_wrapper/NetworkWrapper.scala
@@ -2,6 +2,7 @@ package mpst.frontend.caos_wrapper
 
 import mpst.operational_semantic.Network
 import mpst.operational_semantic.Network.NetworkCausal.ChannelQueue
+import mpst.operational_semantic.Network.NetworkCausalSession.SessionQueue
 import mpst.syntax.Protocol.{Action, Local, Participant}
 import mpst.utility.Environment.Environment
 import mpst.utility.Multiset
@@ -18,27 +19,40 @@ import caos.sos.SOS
 */
 
 object NetworkWrapper:
-    private type CSState = (Set[(Participant, Local)], ChannelQueue, Environment)
+    private type CausalChannelState = (Set[(Participant, Local)], ChannelQueue, Environment)
 
-    object NetworkCausal extends SOS[Action, CSState]:
-      override def accepting(state: CSState): Boolean =
+    object NetworkCausal extends SOS[Action, CausalChannelState]:
+      override def accepting(state: CausalChannelState): Boolean =
         Network.NetworkCausal.accepting(state._1)
       end accepting
 
-      override def next[A >: Action](state: CSState): Set[(A, CSState)] =
+      override def next[A >: Action](state: CausalChannelState): Set[(A, CausalChannelState)] =
         for (nextAction, nextLocalsWithParticipant, nextPending) <- Network.NetworkCausal.next(state._1, state._2)(using state._3) yield
           nextAction -> (nextLocalsWithParticipant, nextPending, state._3)
       end next
     end NetworkCausal
 
-    private type MSState = (Set[(Participant, Local)], Multiset[Action], Environment)
+    private type CausalSessionState = (Set[(Participant, Local)], SessionQueue, Environment)
 
-    object NetworkMultiset extends SOS[Action, MSState]:
-      override def accepting(state: MSState): Boolean =
+    object NetworkSessionCausal extends SOS[Action, CausalSessionState]:
+      override def accepting(state: CausalSessionState): Boolean =
+        Network.NetworkCausalSession.accepting(state._1)
+      end accepting
+
+      override def next[A >: Action](state: CausalSessionState): Set[(A, CausalSessionState)] =
+        for (nextAction, nextLocalsWithParticipant, nextPending) <- Network.NetworkCausalSession.next(state._1, state._2)(using state._3) yield
+          nextAction -> (nextLocalsWithParticipant, nextPending, state._3)
+      end next
+    end NetworkSessionCausal
+
+    private type NonCausalState = (Set[(Participant, Local)], Multiset[Action], Environment)
+
+    object NetworkMultiset extends SOS[Action, NonCausalState]:
+      override def accepting(state: NonCausalState): Boolean =
         Network.NetworkMultiset.accepting(state._1)
       end accepting
 
-      override def next[A >: Action](state: MSState): Set[(A, MSState)] =
+      override def next[A >: Action](state: NonCausalState): Set[(A, NonCausalState)] =
         for (nextAction, nextLocalsWithParticipant, nextPending) <- Network.NetworkMultiset.next(state._1, state._2)(using state._3) yield
           nextAction -> (nextLocalsWithParticipant, nextPending, state._3)
       end next
--- src/main/scala/mpst/operational_semantic/Network.scala
+++ src/main/scala/mpst/operational_semantic/Network.scala
@@ -17,6 +17,40 @@ import scala.collection.immutable.Queue
 */
 
 object Network:
+  object NetworkCausalSession:
+    type SessionQueue = Queue[(Participant, Participant, Label)]
+
+    def accepting(localsWithParticipant: Set[(Participant, Local)]): Boolean =
+      localsWithParticipant.forall{ case _ -> local => MPSTSemantic.accepting(local) }
+    end accepting
+
+    def next[A >: Action](localsWithParticipant: Set[(Participant, Local)], pending: SessionQueue)(using environment: Environment): Set[(A, Set[(Participant, Local)], SessionQueue)] =
+      localsWithParticipant.flatMap( localWithParticipant =>
+        nextEntry(localWithParticipant, pending).map { case (nextAction, nextLocalWithParticipant, nextPending) =>
+          (nextAction, localsWithParticipant - localWithParticipant + nextLocalWithParticipant, nextPending)
+        }
+      )
+    end next
+
+    private def nextEntry(localWithParticipant: (Participant, Local), pending: SessionQueue)(using environment: Environment): Set[(Action, (Participant, Local), SessionQueue)] =
+      def notBlocked(action: Action, pending: SessionQueue): Boolean = action match
+        case _: Send => true
+        case Receive(receiver, sender, label, _) => pending.headOption.contains((sender, receiver, label))
+      end notBlocked
+
+      def nextPending(action: Action, pending: SessionQueue): SessionQueue = action match
+        case Send   (sender, receiver, label, _) => pending.enqueue((sender, receiver, label))
+        case Receive(receiver, sender, label, _) => if pending.headOption.contains((sender, receiver, label))
+          then pending.dequeue._2
+          else pending
+        end nextPending
+
+      for nextAction -> nextLocal <- MPSTSemantic.next(localWithParticipant._2)(using environment(localWithParticipant._1))
+          if notBlocked(nextAction, pending)
+      yield (nextAction, localWithParticipant._1 -> nextLocal, nextPending(nextAction, pending))
+    end nextEntry
+  end NetworkCausalSession
+
   object NetworkCausal:
     type ChannelQueue = Map[(Participant, Participant), Queue[Label]]
 
@@ -25,18 +59,17 @@ object Network:
     end accepting
 
     def next[A >: Action](localsWithParticipant: Set[(Participant, Local)], pending: ChannelQueue)(using environment: Environment): Set[(A, Set[(Participant, Local)], ChannelQueue)] =
-      localsWithParticipant.flatMap {
-        localWithParticipant => nextEntry(localWithParticipant, pending).map {
-          case (nextAction, nextLocalWithParticipant, nextPending) =>
-            (nextAction, localsWithParticipant - localWithParticipant + nextLocalWithParticipant, nextPending)
+      localsWithParticipant.flatMap( localWithParticipant =>
+        nextEntry(localWithParticipant, pending).map { case (nextAction, nextLocalWithParticipant, nextPending) =>
+          (nextAction, localsWithParticipant - localWithParticipant + nextLocalWithParticipant, nextPending)
         }
-      }
+      )
     end next
 
     private def nextEntry(localWithParticipant: (Participant, Local), pending: ChannelQueue)(using environment: Environment): Set[(Action, (Participant, Local), ChannelQueue)] =
       def notBlocked(action: Action, pending: ChannelQueue): Boolean = action match
-        case Send   (_, _, _, _) => true
-        case Receive(receiver, sender, label, sort) =>
+        case _: Send => true
+        case Receive(receiver, sender, label, _) =>
           pending.getOrElse(sender -> receiver, Queue.empty).headOption.contains(label)
       end notBlocked
 
